d015d9d26c7d5c6ba9b606b6f5744cbc17955be1
puck1609 <1609578991@qq.com>
2022-04-08 11:24:35 +0800

    v0.0.1

---
+++ leetcode.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+
+}
+
+// solution
+
+// puck1609-leetcode-go
+// go-util
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
